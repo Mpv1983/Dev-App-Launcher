f3967c61216ced923aebe48e5093ca1b245615d2
Mpv1983 <46942529+Mpv1983@users.noreply.github.com>
2023-12-17 21:27:03 +0000

    Fields update

--- src/pages/manage/manage-app.jsx
+++ src/pages/manage/manage-app.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useContext } from 'react';
 import TextField from '../../component/text-field.jsx';
 import FilePickerField from '../../component/file-picker-field.jsx';
-import { FilePicker } from 'react-file-picker'
-import Modal from '../../component/modal.jsx'
+import handleChange from '../../component/handleChange.js';
 import ServiceProviderContext from '../../contexts/serviceProviderContext.jsx';
 
 export default function ManageApp(props) {
 
+    const [port, SetPort] = useState(0);
+    const [appName, SetAppName] = useState('');
     const { serviceProvider } = useContext(ServiceProviderContext);
+    var file = {name:'', path:'', lastModified:0, lastModifiedDate:{}, size:0, type:''};
 
     function handleFileSelected(fileObject){
-        console.log(fileObject);
+        file = fileObject;
+        SetAppName(file.name);
     }
 
-
     function saveApplication(){
-        serviceProvider.configurationService.addApplication({port:1234, name:'test'})
+        serviceProvider.configurationService.addApplication({port:port, name:appName})
     }
 
     return <div>
@@ -24,8 +26,11 @@ export default function ManageApp(props) {
 
             <FilePickerField id="selectProjectFile" label="Select Project" fileExtension="csproj" onFileSelectedEvent={FileObject => handleFileSelected(FileObject)} />
 
-            <TextField label='Port'/>
+            <div>
+                <TextField label='App Name' value={appName} onChange={(e)=>handleChange(e, SetAppName)}/>
+                <TextField label='Port' value={port} onChange={(e)=>handleChange(e, SetPort)}/>
+            </div>
 
-            <button onClick={saveApplication()}>Save</button>
+            <button onClick={saveApplication} type="button">Save</button>
         </div>;
-  }
+}
