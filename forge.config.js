7dab1aaf31cd59ca523fc591ae35c2b88376f008
Mpv1983 <46942529+Mpv1983@users.noreply.github.com>
2025-04-02 11:01:23 +0100

    App Icon

--- forge.config.js
+++ forge.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: "./src/icons/app_runner_icon"
   },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        setupIcon: "./src/icons/app_runner_icon.ico"
+      },
     },
     {
       name: '@electron-forge/maker-zip',
